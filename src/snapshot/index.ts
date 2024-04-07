816851669bb2fe7acefd8816350a9eae44a1da70
noqcks <benji@093b.org>
2024-04-07 01:38:07 +0000

    Upgrade dependencies

--- src/snapshot/index.ts
+++ src/snapshot/index.ts
@@ -10,7 +10,7 @@ export async function getVSCodeTokens(registry: tm.Registry, scope: string, sour
       throw new Error(`Could not load scope ${scope}`)
     }
 
-    let ruleStack: tm.StackElement | undefined
+    let ruleStack: tm.IStackElement | undefined
 
     return source.split(/\r\n|\n/).map((line: string, n: number) => {
       var { tokens, ruleStack: ruleStack1 } = grammar.tokenizeLine(line, ruleStack)
